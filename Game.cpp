b8818b70da7c0f958b844d2462605883b5a679b0
kemkemg0 <kemkemg0@KENSHIN2AD1>
2022-04-04 00:40:56 -0700

    Add project files.

---
+++ Game.cpp
@@ -0,0 +1,145 @@
+// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+// 
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
+#include "Game.h"
+#include "SDL/SDL_image.h"
+#include <algorithm>
+//#include "Actor.h"
+//#include "SpriteComponent.h"
+//#include "Ship.h"
+//#include "Asteroid.h"
+#include "Random.h"
+ #include "SDL/SDL_mixer.h"
+
+
+Game::Game()
+:mWindow(nullptr)
+,mRenderer(nullptr)
+,mIsRunning(true)
+,mUpdatingActors(false)
+{
+	
+}
+
+
+bool Game::Initialize()
+{
+	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0)
+	{
+		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+		return false;
+	}
+	
+	mWindow = SDL_CreateWindow("Game Programming in C++ (Chapter 3)", 100, 100, 1024, 768, 0);
+	if (!mWindow)
+	{
+		SDL_Log("Failed to create window: %s", SDL_GetError());
+		return false;
+	}
+	
+	mRenderer = SDL_CreateRenderer(mWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (!mRenderer)
+	{
+		SDL_Log("Failed to create renderer: %s", SDL_GetError());
+		return false;
+	}
+	
+	if (IMG_Init(IMG_INIT_PNG) == 0)
+	{
+		SDL_Log("Unable to initialize SDL_image: %s", SDL_GetError());
+		return false;
+	}
+
+	LoadData();
+
+	mTicksCount = SDL_GetTicks();
+
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+	return true;
+}
+
+void Game::RunLoop()
+{
+	while (mIsRunning)
+	{
+		ProcessInput();
+		UpdateGame();
+		GenerateOutput();
+	}
+}
+
+void Game::ProcessInput()
+{
+	SDL_Event event;
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+			case SDL_QUIT:
+				mIsRunning = false;
+				break;
+		}
+	}
+	
+	const Uint8* keyState = SDL_GetKeyboardState(NULL);
+	if (keyState[SDL_SCANCODE_ESCAPE])
+	{
+		mIsRunning = false;
+	}
+
+	mUpdatingActors = true;
+	/*for (auto actor : mActors)
+	{
+		actor->ProcessInput(keyState);
+	}*/
+	mUpdatingActors = false;
+}
+
+void Game::UpdateGame()
+{
+	// Compute delta time
+	// Wait until 16ms has elapsed since last frame
+	while (!SDL_TICKS_PASSED(SDL_GetTicks(), mTicksCount + 16))
+		;
+	float deltaTime = (SDL_GetTicks() - mTicksCount) / 1000.0f;
+	if (deltaTime > 0.05f)deltaTime = 0.05f;
+	mTicksCount = SDL_GetTicks();
+
+	// Update all actors
+	mUpdatingActors = true;
+	/*for (auto actor : mActors)
+	{
+		actor->Update(deltaTime);
+	}*/
+}
+
+void Game::GenerateOutput()
+{
+	//bg?
+	SDL_SetRenderDrawColor(mRenderer, 220, 220, 220, 255);
+	SDL_RenderClear(mRenderer);
+	
+	// Draw all sprite components
+		/*	for (auto sprite : mSprites)
+			{
+				sprite->Draw(mRenderer);
+			}*/
+
+	SDL_RenderPresent(mRenderer);
+}
+
+void Game::Shutdown()
+{
+	IMG_Quit();
+	SDL_DestroyRenderer(mRenderer);
+	SDL_DestroyWindow(mWindow);
+	SDL_Quit();
+}
+
+
+void Game::LoadData(){}
